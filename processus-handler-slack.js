09031a1382e74b9c9db86d2f3be337305b6c1caa
jasonb2 <jason@cloudb2.com>
2015-12-01 08:27:49 -0800

    changed props to params

--- processus-handler-slack.js
+++ processus-handler-slack.js
@@ -3,8 +3,8 @@ var request = require('request');
 /* Processus Handler for Slack incomming webhooks
  * See https://api.slack.com/incoming-webhooks for details on the API
  * Task INPUT
- * @param task.properties.webhookURL The full url assigned to your webhook
- * @param task.properties.payload The payload to be sent to slack. See https://api.slack.com/incoming-webhooks
+ * @param task.parameters.webhookURL The full url assigned to your webhook
+ * @param task.parameters.payload The payload to be sent to slack. See https://api.slack.com/incoming-webhooks
  * for more details on the parameters i.e.
       "username": [the BOT Username to report as e.g. 'processus-bot']
       "channel":  [Channel or user name to message e.g. '#general']
@@ -12,42 +12,42 @@ var request = require('request');
       "icon_url": [URL of icon to show]
       "unfurl_links": [true | false]
  * Task OUTPUT
- * @param task.properties which includes input and request, response and body i.e.
-     task.properties.webhookURL
-     task.properties.payload
-     task.properties.response
-     task.properties.request
-     task.properties.body
+ * @param task.parameters which includes input and request, response and body i.e.
+     task.parameters.webhookURL
+     task.parameters.payload
+     task.parameters.response
+     task.parameters.request
+     task.parameters.body
  */
 module.exports = function(workflowId, taskName, task, callback, logger){
 
   var err;
 
   //Check for presence of the data property
-  if(!task.properties) {
-    callback(new Error("Task [" + taskName + "] has no data property!"), task);
+  if(!task.parameters) {
+    callback(new Error("Task [" + taskName + "] has no parameters property!"), task);
     return;
   }
 
-  if(!task.properties.webhookURL) {
-    callback(new Error("Task [" + taskName + "] has no data.webhookURL property!"), task);
+  if(!task.parameters.webhookURL) {
+    callback(new Error("Task [" + taskName + "] has no parameters.webhookURL property!"), task);
     return;
   }
 
-  if(!task.properties.payload) {
-    callback(new Error("Task [" + taskName + "] has no data.payload property!"), task);
+  if(!task.parameters.payload) {
+    callback(new Error("Task [" + taskName + "] has no parameters.payload property!"), task);
     return;
   }
 
   var options = {};
   options.json = true;
   options.method = "POST";
-  options.url = task.properties.webhookURL;
-  options.body = task.properties.payload;
+  options.url = task.parameters.webhookURL;
+  options.body = task.parameters.payload;
 
   request(options, function (error, response, body) {
-    task.properties.response = response;
-    task.properties.body = body;
+    task.parameters.response = response;
+    task.parameters.body = body;
     callback(error, task);
 
   });
